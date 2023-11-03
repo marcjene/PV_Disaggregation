b001a1a41e383cedfad812c919cd797e644304cc
marcjene <59958349+marcjene@users.noreply.github.com>
2023-11-03 10:32:07 +0100

    Add files via upload

--- aus_data_prep.py
+++ aus_data_prep.py
@@ -18,16 +18,16 @@ def select_customer(dataframe, id_column, customer):
     return dataframe_customer[dataframe_customer[id_column] == customer]
 
 
-def obtain_capacity_postcode(dataframe, capacity_column, pc_column):
+def obtain_capacity_postcode(dataframe_customer, capacity_column, pc_column):
     """
-    Function to select a specific customer of the dataframe.
+    Function to obtain capacity and postcode of a specific customer.
 
-    :param dataframe: df with all the customers
+    :param dataframe_customer: df with all the customers
     :param capacity_column: name of the column including the capacity
     :param pc_column: name of the column including the postcode
     :return: capacity and postcode
     """
-    return dataframe[capacity_column][0], dataframe[pc_column][0]
+    return dataframe_customer[capacity_column][0], dataframe_customer[pc_column][0]
 
 
 def subtract_half_hour(time_str):
@@ -76,6 +76,7 @@ def format_df(dataframe):
     df_consumptions.columns.name = None
     return df_consumptions
 
+
 def energy_to_power(dataframe, columns, granularity):
     """
     Function to convert energy values (in kWh) to power (in kW).
@@ -92,7 +93,7 @@ def energy_to_power(dataframe, columns, granularity):
 def calculate_consumptions(dataframe):
     """
     Function to calculate total consumption 'TC', which combines controlled loads and general consumption, and net
-    consumption 'NC', which represents the readings at the Smart Meter level
+    consumption 'NC', which represents the readings at the Smart Meter level.
     :param dataframe: dataframe with columns 'CL', 'GC', and 'GG'
     :return: dataframe with columns 'TC' and 'NC', apart from 'CL', 'GC', and 'GG'
     """
@@ -131,35 +132,112 @@ def plot_nc_gg(dataframe, starting_day, number_of_days):
     plt.show()
 
 
+def combination_1(dataframe, id_column='Customer', customer=1):
+    """
+    Select one customer, print the capacity and postcode and plot net consumption vs. gross generation and some other
+    interesting data.
+    :param dataframe: Dataframe from Ausgrid dataset
+    :param id_column: Column with customers IDs
+    :param customer: Customer ID to be selected
+    :return: Capacity, postcode, plot of NC and GG, maximum generation, minimum net consumption
+    """
+    df_customer = select_customer(dataframe, id_column, customer)
+    df_customer.reset_index(drop=True, inplace=True)
+
+    PV_capacity, postcode = obtain_capacity_postcode(df_customer, 'Generator Capacity', 'Postcode')
+
+    df_customer = df_customer.copy().drop(columns=['Customer', 'Generator Capacity', 'Postcode', 'Row Quality'])
+
+    # Formatting dataframe to present consumption in columns
+    df_format = format_df(df_customer)
+
+    # Energy magnitudes to power
+    df_power = energy_to_power(df_format, ['CL', 'GC', 'GG'], 0.5)
+
+    # Calculate Total Consumption and Net Consumption
+    df_BTM = calculate_consumptions(df_power)
+    # print(df_BTM)
+
+    # Plot Net Consumption vs. Gross Generation
+    plot_nc_gg(df_BTM, 1, 7)
+
+    print('PV Capacity: ', PV_capacity)
+    print('Postcode: ', postcode)
+
+    # Maximum generation
+    print('Maximum generation: ', df_BTM['GG'].max(), 'kW. At', df_BTM['GG'].idxmax())
+    print('Minimum net consumption: ', df_BTM['NC'].min(), 'kW. At', df_BTM['NC'].idxmin())
+    print('GG when minimum NC: ', df_BTM.loc[pd.Timestamp(df_BTM['NC'].idxmin()), 'GG'])
+
+def count_customers(df, n_appearances):
+    """
+    Return a list of postal codes with more than n customers in the dataset
+    :param df: Dataframe from Ausgrid dataset
+    :param n_appearances: Number of customers from every PC
+    :return: dataframe with PCs and number of customers from that region in the dataframe
+    """
+    # Group by 'Postcode' and count unique 'Customer' values
+    PC_count = df.groupby('Postcode')['Customer'].nunique().reset_index()
+    # Rename the column to 'CustomerCount'
+    PC_count = PC_count.rename(columns={'Customer': 'CustomerCount'})
+
+    print(PC_count[PC_count['CustomerCount'] > n_appearances])
+
+def capacity_interval(df, id_column='Customer', customer=1):
+    """
+    Calculates the minimum capacity of the PV system. Based on article Li2019. The value should be divided by the
+    generation of the standard PV system - or somehow  corrected with meteorological values (TBD).
+    :param df: Dataframe from Ausgrid dataset
+    :param customer: Customer ID
+    :return: minimum capacity
+    """
+    # Select customer to calculate the capacity
+    df_customer = select_customer(df, id_column, customer)
+    df_customer.reset_index(drop=True, inplace=True)
+
+    # Obtain real capacity and drop columns
+    PV_capacity, postcode = obtain_capacity_postcode(df_customer, 'Generator Capacity', 'Postcode')
+    df_customer = df_customer.copy().drop(columns=['Customer', 'Generator Capacity', 'Postcode', 'Row Quality'])
+
+    # Formatting dataframe to present consumption in columns
+    df_format = format_df(df_customer)
+    # Energy magnitudes to power
+    df_power = energy_to_power(df_format, ['CL', 'GC', 'GG'], 0.5)
+    # Calculate Total Consumption and Net Consumption - We'll use the net consumption
+    df_BTM = calculate_consumptions(df_power)
+
+    # Maximum generation is printed to use as benchmark
+    print('Maximum generation: ', df_BTM['GG'].max(), 'kW. At', df_BTM['GG'].idxmax())
+
+    # Minimum capacity calculation
+    C_min = abs(df_BTM['NC'].min())
+    # Day with minimum net consumption
+    date_min = df_BTM['NC'].idxmin()
+    print('Minimum net consumption at: ', date_min)
+
+    # Maximum capacity calculation
+    # Base load calculated during night hours - Hypothesis: 1 am to 6 am
+    # Can it be calculated following a different approach? - Maybe find a daily load curve without generation
+    start_time = date_min.replace(hour=1, minute=0, second=0)
+    end_time = date_min.replace(hour=6, minute=0, second=0)
+    selected_data = df_BTM[(df_BTM.index >= start_time) & (df_BTM.index <= end_time)]
+    print(df_BTM['NC'].loc[start_time])
+    print(df_BTM['NC'].loc[end_time])
+    avg_night = selected_data['NC'].mean()
+
+    C_max = abs(df_BTM['NC'].min()) + avg_night
+    return C_min, C_max, PV_capacity
+
+
 # ======================================== MAIN ========================================
 
 dataset_ausgrid = pd.read_csv('data/Ausgrid/2012-2013 Solar home electricity data v2.csv', header=1)
 
-df_customer = select_customer(dataset_ausgrid, 'Customer', 7)
-df_customer.reset_index(drop=True, inplace=True)
+# print(dataset_ausgrid)
+# combination_1(dataset_ausgrid, 'Customer', 1)
 
-PV_capacity, postcode = obtain_capacity_postcode(df_customer, 'Generator Capacity', 'Postcode')
+# count_customers(dataset_ausgrid, 5)
 
-df_customer = df_customer.copy().drop(columns=['Customer', 'Generator Capacity', 'Postcode', 'Row Quality'])
+C_min, C_max, PV_capacity = capacity_interval(dataset_ausgrid, customer=3)
 
-# Formatting dataframe to present consumption in columns
-df_format = format_df(df_customer)
-
-# Energy magnitudes to power
-df_power = energy_to_power(df_format, ['CL', 'GC', 'GG'], 0.5)
-
-# Calculate Total Consumption and Net Consumption
-df_BTM = calculate_consumptions(df_power)
-print(df_BTM)
-
-# Plot Net Consumption vs. Gross Generation
-plot_nc_gg(df_BTM, 1, 7)
-
-print('PV Capacity: ', PV_capacity)
-print('Postcode: ', postcode)
-
-# Maximum generation
-# print('Maximum generation: ', df_BTM['GG'].max(), 'kW. At', df_BTM['GG'].idxmax())
-# print('Minimum net consumption: ', df_BTM['NC'].min(), 'kW. At', df_BTM['NC'].idxmin())
-# df_BTM.loc[pd.Timestamp('2012-11-11 12:00:00'), 'GG']
-
+print(C_min, C_max, PV_capacity)
